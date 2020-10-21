facb7598c8a52b4812d35dd54dca83ef464db6f5
yoojin94 <bas8520@naver.com>
2020-10-21 14:38:09 +0900

    Updates

---
+++ precache-manifest.4a394f1f36acbbdfdd21d278370e6a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acb2e24230a2b9315c767a34a695949",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3b969806a7d4ae0018f3",
+    "url": "/movie_app/static/css/main.444e8921.chunk.css"
+  },
+  {
+    "revision": "5dd82ef1c354ed1cbe77",
+    "url": "/movie_app/static/js/2.11a22306.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app/static/js/2.11a22306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b969806a7d4ae0018f3",
+    "url": "/movie_app/static/js/main.1e9a5e47.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
